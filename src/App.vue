b57e60cb1e70e97b3c244e9814723bbcdc6bea9d
Jeremy <ayumi2183@gmail.com>
2018-10-25 15:23:49 +0800

    add header nav content for App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-   
+    <!-- header -->
+    <div class="header">
+      header
+    </div>
+
+    <!-- nav -->
+    <div class="nav">
+      nav
+    </div>
+
+    <!-- content -->
+    <div class="content">
+      content
+    </div>
   </div>
 </template>
 
