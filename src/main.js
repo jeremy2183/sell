40cbc1a031c63b9fcb8fa3932989112de03e9609
Jeremy <ayumi2183@gmail.com>
2018-10-25 23:27:19 +0800

    導入組件、定義路由、創建router及掛載router

--- src/main.js
+++ src/main.js
@@ -4,6 +4,39 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 
+//1.導入組件(定義路由組件)
+// 可以從其他文件 import 進來
+import Goods from './components/Goods/Goods'
+import Ratings from './components/Ratings/Ratings'
+import Seller from './components/Seller/Seller'
+
+//2.定義路由
+// 每個路由應該映射一個組件。 其中"component" 可以是
+// 通過 Vue.extend() 創建的組件構造器，
+// 或者，只是一個組件配置對象。
+// 我們晚點再討論嵌套路由。
+const routes = [
+	{
+		path: '/goods',
+		components: Goods
+	},
+	{
+		path: '/ratings',
+		components: Ratings
+	},
+	{
+		path: '/seller',
+		components: Seller
+	}
+]
+
+// 3. 創建 router 實例，然後傳 `routes` 配置
+// 你還可以傳別的配置參數
+const router = new VueRouter({
+	routes
+})
+
+
 //安裝vue-router插件
 Vue.use(VueRouter);
 
@@ -13,5 +46,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  // 4. 創建和掛載根實例
+  router	//於3.已創建
 })
