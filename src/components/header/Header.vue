cf670efbbaa15e4e1dedb3b43b926e8abd88abe5
Jeremy <ayumi2183@gmail.com>
2018-10-28 10:26:21 +0800

    add top,search,more -wrapper

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,6 +1,29 @@
 <template>
 	<div class="header">
-		{{poiInfo.name}}
+			<!-- {{poiInfo.name}} -->
+		<!-- 頂部 -->
+		<div class="top-wrapper">
+			<div class="back-wrapper">
+				<span class="icon-arrow_lift"></span>
+			</div>
+
+			<div class="search-wrapper">
+				<span class="search-icon"></span>
+				<input class="search-bar" type="text" placeholder="搜索店內商品">
+			</div>
+
+			<div class="more-wrapper">
+				<a class="spelling-bt" href="#">拚單</a>
+				<div class="more-bt"></div>
+			</div>
+		</div>
+
+		<!-- 主體內容 -->
+		<div class="content-wrapper"></div>
+
+		<!-- 公告內容 -->
+		<div class="bulletin-wrapper"></div>
+
 	</div>
 </template>
 
@@ -16,5 +39,6 @@
 </script>
 
 <style>
-
+	/* 導入css樣式 */
+	@import url(Header.css);
 </style>
