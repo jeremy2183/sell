a97baa79d36cd86b884ce3ad31b0c471155392ee
Jeremy <ayumi2183@gmail.com>
2018-11-07 16:30:01 +0800

    update Goods

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -1,7 +1,24 @@
 <template>
 	<div class="goods">
 		<!-- 分類 -->
-		<div class="menu-wrapper"></div>
+		<div class="menu-wrapper">
+			<ul>
+				<!-- 專場 -->
+				<li class="menu-item">
+					<p class="text">
+						<img :src="container.tag_icon" v-if="container.tag_icon" class="icon">
+						{{container.tag_name}}
+					</p>
+				</li>
+
+				<li class="menu-item" v-for="item in goods">
+					<p class="text">
+						<img :src="item.icon" v-if="item.icon" class="icon">
+						{{item.name}}
+					</p>
+				</li>
+			</ul>
+		</div>
 
 		<!-- 商品列表 -->
 		<div class="foods-wrapper"></div>
@@ -10,7 +27,7 @@
 
 <script>
 	export default{
-			date(){
+			data(){
 				return{
 					container: {},
 					goods: []
@@ -25,8 +42,8 @@
 	        if(dataSource.code == 0){
 	        	that.container = dataSource.data.container_operation_source;
 	        	that.goods = dataSource.data.food_spu_tags;
-	          console.log(that.container);
-	          console.log(that.goods);
+	          // console.log(that.container);
+	          // console.log(that.goods);
 	        }
 	      })
 	      .catch(function (error) { //出錯處理
