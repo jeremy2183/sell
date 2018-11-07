d089be50a9d2515c5780b0a5b506ddc1693f248d
Jeremy <ayumi2183@gmail.com>
2018-11-07 15:00:37 +0800

    add Goods's created

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -10,7 +10,29 @@
 
 <script>
 	export default{
-
+			date(){
+				return{
+					container: {},
+					goods: []
+				}
+			},
+			created(){  //發起異步請求，獲取數據
+	    var that = this;
+	    //通過axios發起get請求
+	    this.$axios.get('/api/goods')
+	      .then(function (response) { //獲取到數據
+	        var dataSource = response.data;
+	        if(dataSource.code == 0){
+	        	that.container = dataSource.data.container_operation_source;
+	        	that.goods = dataSource.data.food_spu_tags;
+	          console.log(that.container);
+	          console.log(that.goods);
+	        }
+	      })
+	      .catch(function (error) { //出錯處理
+	        console.log(error);
+	      });
+	  }
 	}
 </script>
 
