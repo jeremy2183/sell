143f15cf6979acb8cc45b19d92135d29f329992d
Jeremy <ayumi2183@gmail.com>
2018-11-14 16:14:04 +0800

    add Shorcart computed

--- src/components/Shopcart/Shopcart.vue
+++ src/components/Shopcart/Shopcart.vue
@@ -3,9 +3,11 @@
 		<div class="content-left">
 			<div class="logo-wrapper">
 				<span class="logo icon-shopping_cart"></span>
+				<!-- <i class="num">6</i> -->
 			</div>
 			<div class="desc-wrapper">
-				<p class="tip">另需{{shipping_fee_tip}}</p>
+				<!-- <p class="total-price">¥158</p> -->
+				<p class="tip">{{shipping_fee_tip}}</p>
 			</div>
 		</div>
 		
@@ -25,6 +27,32 @@
 			shipping_fee_tip: {
 				type: String,
 				default: '', 
+			},
+			selectFoods: {
+				type: Array,
+				default(){
+					return [
+
+					];
+				}
+			}
+		},
+		computed: {
+			//總個數
+			totalCount(){
+				let num = 0;
+				this.selectFoods.forEach((food)=>{
+					num += food.count;
+				});
+				return num;
+			},
+			//計算總金額
+			totalPrice(){
+				let total = 0;
+				this.selectFoods.forEach((food)=>{
+					total += food.min_price * food.count; //價格乘個數
+				});
+				return total;
 			}
 		}
 	}
