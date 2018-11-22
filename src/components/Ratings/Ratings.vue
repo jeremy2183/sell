7709cfea496084e65a0eb2d5e65253e39a4f7c16
Jeremy <ayumi2183@gmail.com>
2018-11-22 11:57:14 +0800

    update Ratings

--- src/components/Ratings/Ratings.vue
+++ src/components/Ratings/Ratings.vue
@@ -1,16 +1,40 @@
 <template>
 	<div class="ratings">
-		評價頁面內容
+		<div class="ratings-wrapper">
+			<div class="overview">
+				<div class="overview-left">{{ratings.quality_score}}</div>
+				<div class="overview-right"></div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default{
-
+		data(){
+			return{
+				ratings: {},
+			}
+		},
+		created(){
+			var that = this;
+			//通過axios發起get請求
+    	this.$axios.get('/api/ratings')
+      .then(function (response) { //獲取到數據
+        var dataSource = response.data;
+        if(dataSource.code == 0){
+          that.ratings = dataSource.data;
+        }
+      })
+      .catch(function (error) { //出錯處理
+        console.log(error);
+      });
+		}
 	}
 </script>
 
 <style>
+	@import url(Ratings.css);
 	.ratings{
 		/*text-align: center;*/
 	}
