bbe0fd7930523223ab3f4e2bebdffedff88cd664
Jeremy <ayumi2183@gmail.com>
2018-11-28 16:41:31 +0800

    add seller-wrapper & created & import Split

--- src/components/Seller/Seller.vue
+++ src/components/Seller/Seller.vue
@@ -1,17 +1,71 @@
 <template>
-	<div class="seller">
-		商家頁面內容
+	<div class="seller" ref="sellerView">
+		<div class="seller-wrapper">
+			<Split></Split>
+			<div class="seller-view">
+				<div class="address-wrapper">
+					<div class="address-left">
+						{{seller.address}}
+					</div>
+					<div class="address-right">
+						<div class="content"></div>
+					</div>
+				</div>
+				<div class="pic-wrapper"></div>
+				<div class="safety-wrapper"></div>
+			</div>
+			
+			<Split></Split>
+			<div class="tip-wrapper"></div>
+
+			<Split></Split>
+			<div class="other-wrapper"></div>
+		
+		</div>
+
 	</div>
 </template>
 
 <script>
+	import Split from 'components/Split/Split'
 	export default{
+		data(){
+			return{
+				seller: {}
+			}
+		},
+		created(){
+			var that = this;
+			//通過axios發起get請求
+    	this.$axios.get('/api/seller')
+      .then(function (response) { //獲取到數據
+        var dataSource = response.data;
+        if(dataSource.code == 0){
+          that.seller = dataSource.data;
 
+
+     //      //初始化滾動
+					// that.$nextTick(()=>{
+					// 	if(!that.scroll){
+					// 		that.scroll = new BScroll(that.$refs.sellerView, {
+					// 				click: true
+					// 		});
+					// 	} else {
+					// 		that.scroll.refresh();
+					// 	}
+					// });
+        }
+      })
+      .catch(function (error) { //出錯處理
+        console.log(error);
+      });
+		},
+		components: {
+			Split
+		}
 	}
 </script>
 
 <style>
-	.seller{
-		/*text-align: center;*/
-	}
+	@import url('Seller.css');
 </style>
