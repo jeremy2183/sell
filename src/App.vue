94cccf3e7df5c21f2dae0c06730a5576275e928c
Jeremy <ayumi2183@gmail.com>
2018-11-28 23:57:57 +0800

    add keep-alive(App.vue)

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,9 @@
     </div> -->
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view></router-view>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
 
   </div>
 </template>
